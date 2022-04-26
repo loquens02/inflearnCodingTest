3f1b33a57c00dd2743b21bf34ffd1f749e22e704
loquens02 <datahj@knou.ac.kr>
2022-04-26 19:10:42 +0900

    problem3-진약수-afterAnswerSeeing-O(N) 약수는1로시작한다

--- CodingTestCpp/AA/main-properDivisor.cpp
+++ CodingTestCpp/AA/main-properDivisor.cpp
@@ -2,21 +2,31 @@
 using namespace std;
 
 int main() {
-	int N,tot=0,lastOne;
+	int N,tot=0;//tot=1
 	scanf("%d",&N);
-	for(int i=1; i<N; i++){
+
+	printf("1");
+	for(int i=2; i<N; i++){
 		if(N%i==0){
-			lastOne= i;
-		}
-	}
-	for(int i=1; i<N; i++){
-		if(N%i==0){
-			printf("%d",i);
+			printf(" + %d",i);
 			tot+=i;
-			if(i!=lastOne){
-				printf(" + ");
-			}
 		}		
 	}
-	printf(" = %d",tot);
+	printf(" = %d",tot+1);//tot
 }
+
+//	int lastOne;
+//	for(int i=1; i<N; i++){
+//		if(N%i==0){
+//			lastOne= i;
+//		}
+//	}
+//	for(int i=1; i<N; i++){
+//		if(N%i==0){
+//			printf("%d",i);
+//			tot+=i;
+//			if(i!=lastOne){
+//				printf(" + ");
+//			}
+//		}		
+//	}
